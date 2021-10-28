93aa09405c324f8f50ff62ed533a733f34deebe1
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2021-10-28 23:58:28 +0700

    adding fungsi input dan memindahkan informasi tubes kedalam sebuah folder

---
+++ fungsiInput/commandInput.h
@@ -0,0 +1,6 @@
+
+int commandInput();
+//menerima input dari user menggunakan "mesin" and stuff. pengganti scanf
+//jika input keyboard = "MOVE", mereturn 1
+//dst
+//jika input gada yang sesuai dengan command, return -1
---
+++ fungsiInput/intInput.h
@@ -0,0 +1,3 @@
+
+int intInput();
+//menerima input dari user berupa angka menggunakan "mesin" and stuff. pengganti scanf
