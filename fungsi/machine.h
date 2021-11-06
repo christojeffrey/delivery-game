e9c7c92bddb630212d318bc7d3776248ce969e22
wmk567 <16520402@std.stei.itb.ac.id>
2021-11-06 14:45:44 +0700

    lagi rapihin

---
+++ fungsi/machine.h
@@ -0,0 +1,55 @@
+#ifndef MACHINE_H
+#define MACHINE_H 
+
+#include "../ADT/boolean.h"
+
+#define BLANK ' '
+#define MARK ';'
+#define CAPACITY 100
+
+typedef struct {
+    char contents[CAPACITY];
+    int length;
+}Word;
+
+typedef int Token;
+
+Token currentToken;
+
+boolean endWord;
+boolean endToken;
+
+Word currentWord;
+
+char currentChar;
+
+extern boolean eot;
+
+static FILE *fp;
+static FILE *tape;
+static FILE *tape2;
+static int retval;
+
+void adv();
+
+void ignoreBlank();
+
+void start();
+
+void emptyWord();
+
+void emptyChar();
+
+void copyWord();
+
+void advWord();
+
+void startWord();
+
+void copyToken();
+
+void advToken();
+
+void startToken();
+
+#endif
