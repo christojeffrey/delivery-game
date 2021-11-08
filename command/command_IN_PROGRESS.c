7ab75315b31cd21fd18305124298008c7f4895ca
FelineJTD <16520335@std.stei.itb.ac.id>
2021-11-08 15:37:53 +0700

    add command in progress

--- command/command_IN_PROGRESS.c
+++ command/command_IN_PROGRESS.c
@@ -1,1 +1,19 @@
 // Feli
+#include "command.h"
+#include <stdio.h>
+
+void command_IN_PROGRESS(gameState status) {
+    /* KAMUS LOKAL */
+    // dari gameState status, yang dipakai hanya inProgress
+    Address p;
+    int i;
+    /* ALGORITMA */
+    printf("Pesanan yang sedang diantarkan:\n");
+    p = status.inProgress;
+    i = 1;
+    while(p->next != NULL) {
+        printf("%d. %s Item (Tujuan: %c)\n", i, p->info.item, p->info.dropoff);
+        i++;
+        p = p->next;
+    }
+}
