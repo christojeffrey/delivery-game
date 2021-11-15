28bcd3cda494ea7154d7688237854067e490340b
fadilfauzani <16520180@mahasiswa.itb.ac.id>
2021-11-15 16:48:03 +0700

    fix handle tas kosong di dropoff

--- command/command_DROP_OFF.c
+++ command/command_DROP_OFF.c
@@ -13,21 +13,25 @@ void command_DROP_OFF(gameState* status){
     int uang;
     /* ALGORITMA */
     currLoc = getLoc(status->myLoc, status->buildings);
-    paket = status->tas.buffer[status->tas.idxTop];
-    if (paket.dropoff == currLoc) {
-        if (paket.item == 'N') {
-            uang = 200;
-        } else if (paket.item == 'H') {
-            uang = 400;
-        } else if (paket.item == 'P') {
-            uang = 400;
+    if (status->tas.idxTop != -1){
+        paket = status->tas.buffer[status->tas.idxTop];
+        if (paket.dropoff == currLoc) {
+            if (paket.item == 'N') {
+                uang = 200;
+            } else if (paket.item == 'H') {
+                uang = 400;
+            } else if (paket.item == 'P') {
+                uang = 400;
+            }
+            popBag(&(status->tas), &paket);
+            deleteFirstInProgressList(&(status->inProgress), &paket);
+            status->money += uang;
+            printf("Pesanan %c Item berhasil diantarkan!\n", paket.item);
+            printf("Uang yang didapatkan: %d Yen\n", uang);
+        } else {
+            printf("Tidak ada pesanan yang dapat diantarkan!\n");
         }
-        popBag(&(status->tas), &paket);
-        deleteFirstInProgressList(&(status->inProgress), &paket);
-        status->money += uang;
-        printf("Pesanan %c Item berhasil diantarkan!\n", paket.item);
-        printf("Uang yang didapatkan: %d Yen\n", uang);
     } else {
-        printf("Tidak ada pesanan yang dapat diantarkan!\n");
+        printf("Kamu belum mengambil pesanan apa apa\n");
     }
 }
