8de69c3d7fdf3fd81e5caf43ae49922e78f4ad89
wmk567 <16520402@std.stei.itb.ac.id>
2021-11-16 10:44:30 +0700

    nambah komen

--- fungsi/lettermachine.c
+++ fungsi/lettermachine.c
@@ -5,6 +5,10 @@ FILE *tape;
 static int retval;
 
 void adv(){
+/* memajukan pita sebanyak 1 karakter
+   I.S. : Karakter pertama pada pita = currentChar
+   F.S. : currentChar adalah karakter setelah currentChar yang lama,
+          currentChar adalah MARK dan program menghentikan pembacaan */
     retval = fscanf(tape,"%c",&currentChar);
     if (currentChar == MARK){
         fclose(tape);
@@ -12,6 +16,11 @@ void adv(){
 }
 
 void start(){
+/* memulai pembacaan sebuah input dari pita yang dibaca per karakter
+   I.S. : sembarang
+   F.S. : currentChar adalah karakter pertama dalam pita
+          Jika currentChar != MARK maka pembacaan pita akan dilanjutkan
+          Jika currentChar == MARK maka pembacaan pita akan diberhentikan */
     tape = stdin;
     adv();
 }
