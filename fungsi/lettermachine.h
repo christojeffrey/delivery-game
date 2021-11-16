e1b42e1919726d5146c2d94f60c0e46d9122f3b5
wmk567 <16520402@std.stei.itb.ac.id>
2021-11-16 10:44:33 +0700

    nambah komen

--- fungsi/lettermachine.h
+++ fungsi/lettermachine.h
@@ -11,7 +11,17 @@ extern FILE *tape;
 extern char currentChar;
 
 void adv();
+/* memajukan pita sebanyak 1 karakter
+   I.S. : Karakter pertama pada pita = currentChar
+   F.S. : currentChar adalah karakter setelah currentChar yang lama,
+          currentChar adalah MARK dan program menghentikan pembacaan */
+
 
 void start();
+/* memulai pembacaan sebuah input dari pita yang dibaca per karakter
+   I.S. : sembarang
+   F.S. : currentChar adalah karakter pertama dalam pita
+          Jika currentChar != MARK maka pembacaan pita akan dilanjutkan
+          Jika currentChar == MARK maka pembacaan pita akan diberhentikan */
 
 #endif
