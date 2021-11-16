d4bf35bc5068131d725e6ca5fe3be2422eedea4d
wmk567 <16520402@std.stei.itb.ac.id>
2021-11-16 23:09:21 +0700

    perbaikan lagi

--- fungsi/isContinue.c
+++ fungsi/isContinue.c
@@ -1,9 +1,11 @@
 #include "../ADT/boolean.h"
 #include "../ADT/gameState.h"
+#include "../ADT/todoList.h"
+#include "../ADT/bag.h"
 boolean isContinue(gameState status){
     boolean lanjut;
     lanjut = true;
-    if(status.orders.idxHead == -1 && status.orders.idxTail == -1){
+    if(status.orders.idxHead == -1 && status.orders.idxTail == -1 && isTodoListEmpty(status.todos) && isBagEmpty(status.tas)){
         lanjut = false;
     }
     return lanjut;
