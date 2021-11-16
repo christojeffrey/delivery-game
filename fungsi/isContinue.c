ed3298b94e35dc66260cbd67e0ed5c6546308185
wmk567 <16520402@std.stei.itb.ac.id>
2021-11-16 21:55:08 +0700

    perbaikan sedikit

--- fungsi/isContinue.c
+++ fungsi/isContinue.c
@@ -2,6 +2,7 @@
 #include "../ADT/gameState.h"
 boolean isContinue(gameState status){
     boolean lanjut;
+    lanjut = true;
     if(status.orders.idxHead == -1 && status.orders.idxTail == -1){
         lanjut = false;
     }
