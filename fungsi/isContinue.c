2c266833682b8bbe6ff0fce26b04a7cdfaf50194
wmk567 <16520402@std.stei.itb.ac.id>
2021-11-17 23:42:52 +0700

    nambah print sedikit

--- fungsi/isContinue.c
+++ fungsi/isContinue.c
@@ -2,11 +2,13 @@
 #include "../ADT/gameState.h"
 #include "../ADT/todoList/todoList.h"
 #include "../ADT/bag/bag.h"
+#include <stdio.h>
 boolean isContinue(gameState status){
     boolean lanjut;
     lanjut = true;
     if(status.orders.idxHead == -1 && status.orders.idxTail == -1 && isTodoListEmpty(status.todos) && isBagEmpty(status.tas)){
         lanjut = false;
+        printf("game selesai");
     }
     return lanjut;
 }
