1a71d4e1ccc5b676e346b669eaeeabe1e5ca6a9a
wmk567 <16520402@std.stei.itb.ac.id>
2021-11-17 16:03:45 +0700

    perbaikan lagi

--- fungsi/commandInput.c
+++ fungsi/commandInput.c
@@ -22,39 +22,39 @@ int commandInput(){
     /* ALGORITMA */
     startWord();
     /* MOVE */
-    if (currentWord.contents[0] == 'M' && currentWord.contents[1] == 'O' && currentWord.contents[2] == 'V' && currentWord.contents[3] == 'E'){
+    if (currentWord.contents[0] == 'M' && currentWord.contents[1] == 'O' && currentWord.contents[2] == 'V' && currentWord.contents[3] == 'E' && currentWord.length == 4){
         result = 1;
     }
     /* PICK_UP */
-    else if (currentWord.contents[0] == 'P' && currentWord.contents[1] == 'I' && currentWord.contents[2] == 'C' && currentWord.contents[3] == 'K' && currentWord.contents[4] == '_' && currentWord.contents[5] == 'U' && currentWord.contents[6] == 'P'){
+    else if (currentWord.contents[0] == 'P' && currentWord.contents[1] == 'I' && currentWord.contents[2] == 'C' && currentWord.contents[3] == 'K' && currentWord.contents[4] == '_' && currentWord.contents[5] == 'U' && currentWord.contents[6] == 'P' && currentWord.length == 7){
         result = 2;
     }
     /* DROP_OFF */
-    else if(currentWord.contents[0] == 'D' && currentWord.contents[1] == 'R' && currentWord.contents[2] == 'O' && currentWord.contents[3] == 'P' && currentWord.contents[4] == '_' && currentWord.contents[5] == 'O' && currentWord.contents[6] == 'F' && currentWord.contents[7] == 'F'){
+    else if(currentWord.contents[0] == 'D' && currentWord.contents[1] == 'R' && currentWord.contents[2] == 'O' && currentWord.contents[3] == 'P' && currentWord.contents[4] == '_' && currentWord.contents[5] == 'O' && currentWord.contents[6] == 'F' && currentWord.contents[7] == 'F' && currentWord.length == 8){
         result = 3;
     }
     /* MAP */
-    else if (currentWord.contents[0] == 'M' && currentWord.contents[1] == 'A' && currentWord.contents[2] == 'P'){
+    else if (currentWord.contents[0] == 'M' && currentWord.contents[1] == 'A' && currentWord.contents[2] == 'P' && currentWord.length == 3){
         result = 4;
     }
     /* TO_DO */
-    else if (currentWord.contents[0] == 'T' && currentWord.contents[1] == 'O' && currentWord.contents[2] == '_' && currentWord.contents[3] == 'D' && currentWord.contents[4] == 'O'){
+    else if (currentWord.contents[0] == 'T' && currentWord.contents[1] == 'O' && currentWord.contents[2] == '_' && currentWord.contents[3] == 'D' && currentWord.contents[4] == 'O' && currentWord.length == 5){
         result = 5;
     }
     /* IN_PROGRESS */
-    else if(currentWord.contents[0] == 'I' && currentWord.contents[1] == 'N' && currentWord.contents[2] == '_' && currentWord.contents[3] == 'P' && currentWord.contents[4] == 'R' && currentWord.contents[5] == 'O' && currentWord.contents[6] == 'G' && currentWord.contents[7] == 'R' && currentWord.contents[8] == 'E' && currentWord.contents[9] == 'S' && currentWord.contents[10] == 'S'){
+    else if(currentWord.contents[0] == 'I' && currentWord.contents[1] == 'N' && currentWord.contents[2] == '_' && currentWord.contents[3] == 'P' && currentWord.contents[4] == 'R' && currentWord.contents[5] == 'O' && currentWord.contents[6] == 'G' && currentWord.contents[7] == 'R' && currentWord.contents[8] == 'E' && currentWord.contents[9] == 'S' && currentWord.contents[10] == 'S' && currentWord.length == 11){
         result = 6;
     }
     /* BUY */
-    else if (currentWord.contents[0] == 'B' && currentWord.contents[1] == 'U' && currentWord.contents[2] == 'Y'){
+    else if (currentWord.contents[0] == 'B' && currentWord.contents[1] == 'U' && currentWord.contents[2] == 'Y' && currentWord.length == 3){
         result = 7;
     }
     /* INVENTORY */
-    else if(currentWord.contents[0] == 'I' && currentWord.contents[1] == 'N' && currentWord.contents[2] == 'V' && currentWord.contents[3] == 'E' && currentWord.contents[4] == 'N' && currentWord.contents[5] == 'T' && currentWord.contents[6] == 'O' && currentWord.contents[7] == 'R' && currentWord.contents[8] == 'Y'){
+    else if(currentWord.contents[0] == 'I' && currentWord.contents[1] == 'N' && currentWord.contents[2] == 'V' && currentWord.contents[3] == 'E' && currentWord.contents[4] == 'N' && currentWord.contents[5] == 'T' && currentWord.contents[6] == 'O' && currentWord.contents[7] == 'R' && currentWord.contents[8] == 'Y' && currentWord.length == 9){
         result = 8;
     }
     /* HELP */
-    else if (currentWord.contents[0] == 'H' && currentWord.contents[1] == 'E' && currentWord.contents[2] == 'L' && currentWord.contents[3] == 'P'){
+    else if (currentWord.contents[0] == 'H' && currentWord.contents[1] == 'E' && currentWord.contents[2] == 'L' && currentWord.contents[3] == 'P' && currentWord.length == 4){
         result = 9;
     }
     else {
