1b2b6ff5bb0b2cb4f2a13825194b42812ea765b0
wmk567 <16520402@std.stei.itb.ac.id>
2021-11-17 20:38:56 +0700

    perbaikan sedikit

--- command/command_PICK_UP.c
+++ command/command_PICK_UP.c
@@ -13,34 +13,38 @@ void command_PICK_UP(gameState* status) {
     boolean found;
     /* ALGORITMA */
     currLoc = getLoc(status->myLoc, status->buildings);
-    p = status->todos;
-    found = false;
-    if (p->info.pickup == currLoc){
-        found = true;
-        deleteFirstTodoList(&(status->todos),&paket);
-    }
-    else {
-        while((p)->next != NULL && !found) {
-            if ((p->next)->info.pickup == currLoc) {
-                paket = (p->next)->info;
-                (p)->next = ((p)->next)->next;
-                found = true;
-            } else {
-                p = (p)->next;
+    if (isBagFull(status->tas)){
+        printf("Tas sudah penuh\n");
+    } else {
+        p = status->todos;
+        found = false;
+        if (p->info.pickup == currLoc){
+            found = true;
+            deleteFirstTodoList(&(status->todos),&paket);
+        }
+        else {
+            while((p)->next != NULL && !found) {
+                if ((p->next)->info.pickup == currLoc) {
+                    paket = (p->next)->info;
+                    (p)->next = ((p)->next)->next;
+                    found = true;
+                } else {
+                    p = (p)->next;
+                }
             }
         }
-    }
 
-    if (p->info.pickup == currLoc){
-        found = true;
-        deleteLastTodoList(&(status->todos),&paket);
-    }
+        if (p->info.pickup == currLoc){
+            found = true;
+            deleteLastTodoList(&(status->todos),&paket);
+        }
 
-    if (found) {
-        pushBag(&(status->tas), paket);
-        insertFirstInProgressList(&(status->inProgress), paket);
-        printf("Pesanan berupa %s Item berhasil diambil!\n", getType(paket));
-    } else {
-        printf("Pesanan tidak ditemukan!\n");
+        if (found) {
+            pushBag(&(status->tas), paket);
+            insertFirstInProgressList(&(status->inProgress), paket);
+            printf("Pesanan berupa %s Item berhasil diambil!\n", getType(paket));
+        } else {
+            printf("Pesanan tidak ditemukan!\n");
+        }
     }
 }
